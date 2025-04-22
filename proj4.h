1766db2c721418940d7711277a7e016fbf52670b
jakecar47 <165412720+jakecar47@users.noreply.github.com>
2025-04-22 13:38:58 -0400

    Update proj4.h

--- proj4.h
+++ proj4.h
@@ -7,7 +7,6 @@
  * the heap of this process. Once this is initialized properly,
  * p[i][j] should be a valid unsigned char for all i = 0..(n-1)
  * and j = 0..(n-1).
- * Do not alter this typedef or struct in any way.
  */
 typedef struct grid_t {
   unsigned int n;
@@ -20,7 +19,6 @@ typedef struct grid_t {
  * is a name of file in the present working directory
  * that contains a valid n-by-n grid of digits, where each
  * digit in the grid is between 1 and 9 (inclusive).
- * Do not alter this function prototype in any way.
  */
 void initializeGrid(grid * g, char * fileName);
 
@@ -33,8 +31,7 @@ void initializeGrid(grid * g, char * fileName);
  * computations involved in calculating the diagonal sums. 
  * This function should call (or call another one of your functions that calls)
  * pthread_create and pthread_join when 2 <= t <= 3 to create additional POSIX
- * thread(s) to compute all diagonal sums. 
- * Do not alter this function prototype in any way.
+ * thread(s) to compute all diagonal sums.
  */
 void diagonalSums(grid * input, unsigned long s, grid * output, int t);
 
@@ -46,7 +43,6 @@ void diagonalSums(grid * input, unsigned long s, grid * output, int t);
  * If fileName does not exist in the present working directory,
  * then this function should create a new file named fileName
  * and assign read and write permissions to the owner. 
- * Do not alter this function prototype in any way.
  */
 void writeGrid(grid * g, char * fileName);
 
@@ -54,15 +50,8 @@ void writeGrid(grid * g, char * fileName);
 /*
  * Free up all dynamically allocated memory used by g.
  * This function should be called when the program is finished using g.
- * Do not alter this function prototype in any way.
  */
 void freeGrid(grid * g);
-
-/*
- * You may add any additional function prototypes and any additional
- * things you need to complete this project below this comment. 
- * Anything you add to this file should be commented. 
- */
 
 
 /*
